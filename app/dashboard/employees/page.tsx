8006a08bcc2ddb5811438da75065bbde60be12c3
fedemoretto11 <fedemoretto94@gmail.com>
2024-01-11 16:57:00 -0300

    Agregado search bar

--- app/dashboard/employees/page.tsx
+++ app/dashboard/employees/page.tsx
@@ -1,10 +1,18 @@
+import Search from "@/app/ui/Search";
 
 export default function Page() {
 
 
   return (
-    <main>
-      <h1>Empleados</h1>
-    </main>
+    <section className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-3xl">Empleados</h1>
+      </div>
+
+      <div>
+        <Search placeholder="Buscar empleado"/>
+
+      </div>
+    </section>
     )
 }
