523e54cbd1abbe8ba54cc76d5c6db187066629e0
fedemoretto11 <fedemoretto94@gmail.com>
2024-01-11 17:47:45 -0300

    Avance en empleados

--- app/dashboard/employees/page.tsx
+++ app/dashboard/employees/page.tsx
@@ -1,4 +1,6 @@
 import Search from "@/app/ui/Search";
+import AddEmployee from "@/app/ui/buttons";
+import Table from "@/app/ui/employee/Table";
 
 export default function Page() {
 
@@ -9,10 +11,11 @@ export default function Page() {
         <h1 className="text-3xl">Empleados</h1>
       </div>
 
-      <div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Buscar empleado"/>
-
+        <AddEmployee />
       </div>
+      <Table />
     </section>
     )
 }
